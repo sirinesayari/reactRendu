897f6921bfe2299d07a8567b8b69ebe8956b0fdb
sirinesayari <sayari.sirine@esprit.tn>
2024-03-02 12:15:23 +0100

    atelier

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Events from "./Components/Events";
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./Components/NavBar";
+import EventDetails from "./Components/EventDetails";
+import AddEvent from "./Components/AddEvent";
+import EditEvents from "./Components/EditEvents";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+    <NavBar/>
+      <Routes >
+        <Route path="/events">
+        <Route index element={<Events/>} />
+        <Route path="Add" element={<AddEvent/>} />
+        <Route path="details/:id" element={<EventDetails/>} />
+        <Route path="update/:id" element={<AddEvent/>}  />
+
+        </Route>
+        {/* <Route path="details/:nom" element={<EventDetails/>} /> */}
+       
+
+        <Route path="*" element={<><p>Not found</p></>} />
+      </Routes>
+     
+    </>
+  );
+}
+
+export default App;
+
